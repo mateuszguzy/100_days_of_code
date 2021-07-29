636a06bffc2fd85d97de291577e025d63da56001
Mateusz Guzy <mateuszguzy6@gmail.com>
2021-07-29 18:38:10 +0200

    Day_1

---
+++ Day_1/Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Weclome to the band name generator!")
+city = input("What's the name of the city, you grew up in?\n")
+color = input("What's the colour of your shirt?\n")
+print("Your band name could be: %s" % city, color)
