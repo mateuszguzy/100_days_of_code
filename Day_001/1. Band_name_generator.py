a62c9dfcb53a7ec00d4d56dcffbfa7f331b57821
Mateusz Guzy <mateuszguzy6@gmail.com>
2021-08-11 11:01:05 +0200

    name change

--- Day_001/1. Band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("Weclome to the band name generator!")
-city = input("What's the name of the city, you grew up in?\n")
-color = input("What's the colour of your shirt?\n")
-print("Your band name could be: %s" % city, color)
--- Day_002/2. Tip_calculator.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to tip calculator")
-total_bill = float(input("How much was the bill? $ "))
-people = int(input("How many people to split the bill? "))
-tip = float(int(input("What percentage tip would you like to give? 10, 12 or 15? ")) / 10)
-value = (total_bill * tip) / people
-print("Each person should pay %s" % value)
