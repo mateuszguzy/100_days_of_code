3b39da79421a7de57cd84ad4f0fbc8cd93915773
Mateusz Guzy <mateuszguzy6@gmail.com>
2021-08-31 20:21:54 +0200

    Day_024

--- Day_024/main_024_Mail_merge.py
+++ Day_024/main_024_Mail_merge.py
@@ -1,8 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import re
+
+
+def main():
+    # prepare regex to match placeholder inside form
+    reg_ex = '\[name\]'
+    # open name list and for each name make following operations
+    with open('Input/Names/invited_names.txt') as name_file:
+        for name in name_file:
+            # open letter form
+            with open('Input/Letters/starting_letter.txt') as letter:
+                # and for each line of letter form
+                for line in letter:
+                    # search for placeholder and replace it with a name and write each file line by line
+                    with open(f'Output/ReadyToSend/letter_to_{name}.txt', mode='a') as letter_to_send:
+                        letter_to_send.write(re.sub(reg_ex, name.rstrip(), line))
+
+
+if __name__ == "__main__":
+    main()
