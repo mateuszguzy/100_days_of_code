1e934213cc34abf9bb3c92c0c44c2304f9dca35d
Mateusz Guzy <mateuszguzy6@gmail.com>
2021-09-11 13:19:27 +0200

    Day_026_update

--- Day_026/main_026.py
+++ Day_026/main_026.py
@@ -3,7 +3,21 @@ import pandas
 code_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 code_dict = {row.letter: row.code for (index, row) in code_data.iterrows()}
 
-name = input("Write your name: ")
 
-code_list = [code_dict[letter.upper()] for letter in name]
-print(code_list)
+def main():
+    name = input("Write your name: ")
+
+    try:
+        code_list = [code_dict[letter.upper()] for letter in name]
+        print(code_list)
+
+    except KeyError:
+        print("Do not insert numbers.")
+        main()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
