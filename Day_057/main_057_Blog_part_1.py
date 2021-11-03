d732b7288bb783694196685e88da07a4268bb561
Mateusz Guzy <mateuszguzy6@gmail.com>
2021-11-03 18:40:39 +0100

    Day_057

---
+++ Day_057/main_057_Blog_part_1.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get(url="https://api.npoint.io/9ac20c33562e438c2d92")
+    # load all posts, and render their titles and subtitles on home page
+    blog_data = response.json()
+    return render_template("index.html", blog_data=blog_data)
+
+@app.route('/post/<int:num>')
+def get_post(num):
+    response = requests.get(url="https://api.npoint.io/9ac20c33562e438c2d92")
+    blog_data = response.json()
+    # on home page there are redirects to exact post
+    # number of post to show is determined by "num" variable and inside JSON in "id" attribute
+    # check all posts on the blog and if one's id match "num" variable pass it into HTML template
+    for entry in blog_data:
+        if entry["id"] == num:
+            post_data = entry
+    return render_template("post.html", post_data=post_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Day_057/test.py
@@ -0,0 +1,22 @@
+a = [
+  {
+    "id": 1,
+    "text": "Text_1",
+    "title": "Title_1",
+    "subtitle": "Subtitle_1"
+  },
+  {
+    "id": 2,
+    "text": "Text_2",
+    "title": "Title_2",
+    "subtitle": "Subtitle_2"
+  },
+  {
+    "id": 3,
+    "text": "Text_3",
+    "title": "Title_3",
+    "subtitle": "Subtitle_3"
+  }
+]
+
+print(a[])
